2bf144b23adc609fa38e4b6a50358c02df7ff6c9
Soumyo Jeet <samajdarsoumyajeet0@gmail.com>
2025-04-04 21:51:46 +0530

    AssistAI initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
+import Explorer from "@/components/Explorer";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +23,35 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+    <html lang="en" className="dark" style={{colorScheme: "dark"}} suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            >
+            {/* header */}
+            <Header />
+
+            <main className="min-h-screen">
+            {children}
+            </main>
+
+            <Toaster richColors/>
+            
+            {/* footer */}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-yellow-200">
+                <p>Made by SoumyoJeet with ❤️</p>
+              </div>
+            </footer>
+          </ThemeProvider>
       </body>
     </html>
+  </ClerkProvider>
   );
 }
